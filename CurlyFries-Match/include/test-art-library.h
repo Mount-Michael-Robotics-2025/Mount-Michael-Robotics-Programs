58ca4519a3d708817fda0e7a8c682bef0fb7c75e
CurlyFries1970 <michael.a.freilich@gmail.com>
2024-01-28 12:59:38 -0600

    update art library

--- CurlyFries-Match/include/test-art-library.h
+++ CurlyFries-Match/include/test-art-library.h
@@ -2,15 +2,4 @@
 
 using namespace vex;
 
-namespace brainArt {
-    class buttonHorizontal {
-        public:
-            int xPosition;
-            int yPosition;
-            int length;
-            int radius;
-            void press();
-
-            buttonHorizontal(int _xPosition)
-    };
-}
+//Empty for now
